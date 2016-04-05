0c2c8bb105256c9bfe3fe03baecafa9bc68df447
fvcproductions <fvcproductions@gmail.com>
2016-04-05 13:37:30 -0400

    add MIT LICENSE
    Signed-off-by: fvcproductions <fvcproductions@gmail.com>

--- CSC-308/Python/BubbleSort.py
+++ CSC-308/Python/BubbleSort.py
@@ -1,12 +1,27 @@
+# FVCproductions
+# Python Bubble Sort Program
+# CSC 308
+
 def BubbleSort(array):
+  # print unsorted array
   print("Unsorted Array")
-  print(array)
+  buffer = ""
+  for i in range(len(array)):
+    buffer = buffer + str(array[i]) + " "
+  print(buffer)
+  # bubble sort
   for i in range(len(array)-1, 0, -1):
     for j in range(i):
       if array[j] > array[j+1]:
         array[j], array[j + 1] = array[j + 1], array[j]
-  print("Sorted Array")
-  print(array)
+  # print sorted array
+  print("\nSorted Array")
+  buffer = ""
+  for i in range(len(array)):
+    buffer = buffer + str(array[i]) + " "
+  print(buffer)
+  # input message
+  input("\nPress any key to continue . . .")
 
-
+# calling BubbleSort function with given numbers
 BubbleSort([ 34, 56, 4, 10, 77, 51, 93, 30, 5, 52 ])
